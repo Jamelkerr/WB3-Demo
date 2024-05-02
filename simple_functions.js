dd4e03951081f1562ec94c44ab1824eca4711bb7
jamel kerr <jamel.kerr@yahoo.com>
2024-05-02 10:13:21 -0400

    favorite

---
+++ simple_functions.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+function favoriteThings() {
+    console.log("jamel");
+    console.log("scarface");
+    console.log("Bôa");
+}
+
+
+function whyImHere() {
+    console.log("just following my dreams i always wanted to be a game designer ");
+}
+
+function favoritePlace() {
+    console.log(" california, because it's so beautiful always sunny.");
+}
+
+
+favoriteThings();
+whyImHere();
+favoritePlace();
